ff58ae5922f1345ce0f3e9523847293ae5e02267
LvBay <570453280@qq.com>
2017-01-07 16:56:08 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	_ "myweb/controllers"
+	_ "myweb/router"
+	"net/http"
+)
+
+func main() {
+	err := http.ListenAndServe(":9091", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
